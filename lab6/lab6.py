c1968d9f61237f813074da38aee050d54ad0e5c4
GekK9 <110110744+GekK9@users.noreply.github.com>
2025-05-26 02:10:43 +0800

    added fixed lab 6

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,12 +1,12 @@
-import scipy
+import numpy
 from scipy.optimize import linprog
 
 
-A_ub = [[3, 2], [5, 3], [2, 4]]
-c = [45, 49, 42]
+A_ub = [[4, 3], [2, 5], [4, 2]]
+c = [45, 45, 29]
 B_ub = [-5, -9]
 
-A_ub_T = -scipy.transpose(A_ub)
+A_ub_T = -numpy.transpose(A_ub)
 res = linprog(c, A_ub_T, B_ub)
 
 print(res)
